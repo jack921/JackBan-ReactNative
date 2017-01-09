130f09a2edab48312fe512c004fc7f782ced0f0e
jack921 <355596943@qq.com>
2017-01-10 01:55:25 +0800

    init

--- android/app/src/main/java/com/jackban/MainApplication.java
+++ android/app/src/main/java/com/jackban/MainApplication.java
@@ -9,7 +9,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
+import com.oblador.vectoricons.VectorIconsPackage;
 import java.util.Arrays;
 import java.util.List;
 
@@ -24,7 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),new VectorIconsPackage()
       );
     }
   };
--- js/main/MainPage.js
+++ js/main/MainPage.js
@@ -13,55 +13,66 @@ import {
 import px2pd from '../util/px2dp.js';
 import Icon from 'react-native-vector-icons/Ionicons';
 import TabNavigator from 'react-native-tab-navigator';
+import StartView from './StartView.js';
 
 class MainPages extends Component{
 
-     static defaultProps = {
+    static defaultProps = {
         selectedColor: 'rgb(22,131,251)',
         normalColor: '#a9a9a9'
     };
-
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            selectedTab:'home',
+        this.state = {
+            selectedTab: 'home',
+            tabName: ['首页', '发现', '我']
         }
     }
 
-    render(){
-
-        return(
-           <View style={styles.container}>
-               <TabNavigator
-                    hidesTabTouch={true}
-                    tabBarStyle={styles.tab}
-                    sceneStyle={{ paddingBottom: styles.tabbar.height }}>
-                  {renderTabItem(this.state.homeNormal,this.state.homeSelected,'首页',createChildView('首页1'))}
-                  {renderTabItem(this.state.compassNormal,this.state.compassSelected,'发现',createChildView('首页2'))}
-                  {renderTabItem(this.state.notificationNormal,this.state.notificationSelected,'消息',createChildView('首页3'))}
-                  {renderTabItem(this.this.state.meNormal,this.state.meSelected,'我',createChildView('首页4'))}
-               </TabNavigator>
-           </View>     
+    render() {
+        const {selectedColor} = this.props;
+        const {tabName} = this.state;
+        return (
+            <TabNavigator
+                hidesTabTouch={true}
+                tabBarStyle={styles.tabbar}
+                sceneStyle={{ paddingBottom: styles.tabbar.height }}>
+                <TabNavigator.Item
+                    tabStyle={styles.tabStyle}
+                    title={tabName[0]}
+                    selected={this.state.selectedTab === 'home'}
+                    selectedTitleStyle={{color: selectedColor}}
+                    renderIcon={() => <Image style={styles.tab} source={this.state.homeNormal} />}
+                    renderSelectedIcon={() => <Image style={styles.tab} source={this.state.homeSelected} />}
+                    onPress={() => this.setState({ selectedTab: 'home' })}>
+                    {<StartView />}
+                </TabNavigator.Item>
+                <TabNavigator.Item
+                    tabStyle={styles.tabStyle}
+                    title={tabName[1]}
+                    selected={this.state.selectedTab === 'compass'}
+                    selectedTitleStyle={{color: selectedColor}}
+                    renderIcon={() => <Image style={styles.tab} source={this.state.compassNormal} />}
+                    renderSelectedIcon={() => <Image style={styles.tab} source={this.state.compassSelected} />}
+                    onPress={() => this.setState({ selectedTab: 'compass' })}>
+                    {<StartView />}
+                </TabNavigator.Item>
+                <TabNavigator.Item
+                    tabStyle={styles.tabStyle}
+                    title={tabName[2]}
+                    selected={this.state.selectedTab === 'me'}
+                    selectedTitleStyle={{color: selectedColor}}
+                    renderIcon={() => <Image style={styles.tab} source={this.state.meNormal} />}
+                    renderSelectedIcon={() => <Image style={styles.tab} source={this.state.meSelected} />}
+                    onPress={() => this.setState({ selectedTab: 'me' })}>
+                    {<StartView />}
+                </TabNavigator.Item>
+            </TabNavigator>
         );
     }
 
-    renderTabItem(img,selectimg,title,childview){
-        return(
-            <TabNavigator.Item
-                selected={this.state.selectedTab===title}
-                title={title}
-                renderIcon={()=><Image style={styles.tabImage} source={img}></Image>}>}
-                renderSelectedIcon={()=><Image style={styles.tabImage} source={selectimg}></Image>}
-                onPress={()=>this.setState({selectedTab:title})}>
-                {childview}
-            </TabNavigator.Item>
-        )
-    }
-
-    componentWillMount(){
+    componentWillMount() {
         const {selectedColor, normalColor} = this.props;
-        Icon.getImageSource('md-notifications', 50, normalColor).then((source) => this.setState({ notificationNormal: source }));
-        Icon.getImageSource('md-notifications', 50, selectedColor).then((source) => this.setState({ notificationSelected: source }));
         Icon.getImageSource('md-home', 50, normalColor).then((source) => this.setState({ homeNormal: source }));
         Icon.getImageSource('md-home', 50, selectedColor).then((source) => this.setState({ homeSelected: source }));
         Icon.getImageSource('md-person', 50, normalColor).then((source) => this.setState({ meNormal: source }));
@@ -69,33 +80,24 @@ class MainPages extends Component{
         Icon.getImageSource('md-compass', 50, normalColor).then((source) => this.setState({ compassNormal: source }));
         Icon.getImageSource('md-compass', 50, selectedColor).then((source) => this.setState({ compassSelected: source }));
     }
-
- createChildView(tag) {  
-    return (  
-        <View style={{flex:1,backgroundColor:'#00baff',alignItems:'center',justifyContent:'center'}}>  
-            <Text style={{fontSize:22}}>{tag}</Text>  
-        </View>  
-    )  
-}  
+ 
 
 }
 
-const styles=StyleSheet.create({
-      container:{
-          flex:1,    
-          flexDirection:'row',
-          justifyContent:'center',      
-      },
-      tabImage:{
-          width:px2pd(22),
-          height:px2pd(22)
-      },
-      tab:{
-          height:px2pd(49),
-          alignItems:'center',
-          justifyContent:'center',
-          backgroundColor:'#fff'  
-      },
+const styles = StyleSheet.create({
+    tabbar: {
+        height: 49,
+        alignItems:'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff'
+    },
+    tabStyle:{
+        padding: 8
+    },
+    tab: {
+        width: 22,
+        height: 22
+    }
 });
 
 export default MainPages;
