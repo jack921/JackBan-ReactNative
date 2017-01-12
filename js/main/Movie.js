82b9f4c64f9cb8b58050f145c166758b69407417
jack921 <355596943@qq.com>
2017-01-12 18:31:25 +0800

    init

--- js/main/Movie.js
+++ js/main/Movie.js
@@ -19,25 +19,16 @@ class Movie extends Component{
 
     constructor(props){
         super(props);
-        this.state={
-             tabName:['热映榜','TOP250']   
-        }
     }
-
      render(){
        return(
           <ScrollableTabView
-            initialPage={1}
             renderTabBar={() => <DefaultTabBar/>}>
-
                 <MovieHotSearch tabLabel='热映榜'></MovieHotSearch>
-                    
                 <MovieTop250 tabLabel='TOP250'></MovieTop250>
-
           </ScrollableTabView>   
        );
     }
-
 }
 
 export default Movie;
--- js/main/MovieHotSearch.js
+++ js/main/MovieHotSearch.js
@@ -7,21 +7,115 @@ import {
     StyleSheet,
     View,
     Text,
-    Image
+    Image,
+    ListView,
+    TouchableOpacity
 }from 'react-native';
+
+var MOVIEHOT='https://api.douban.com/v2/movie/in_theaters';
+var Dimensions = require('Dimensions');
+var {width} = Dimensions.get('window');
+var {height} = Dimensions.get('window');
 
 class MovieHotSearch extends Component{
 
+    constructor(props){
+        super(props);
+        this.state={
+            data:'jack',
+            isLoad:true,
+            dataSource: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2
+            }),    
+        };
+    }
+
     render(){
+        if(this.state.isLoad){
+            return this.renderLoadingView();    
+        }
         return(
-         <View style={{flex:1,backgroundColor:'#00baff',alignItems:'center',justifyContent:'center'}}>  
-                <Text style={{fontSize:22}}>{'热搜'}</Text>  
+          <View style={styles.container}>  
+            <ListView
+                dataSource={this.state.dataSource}
+                renderRow={this.renderListViewItem.bind(this)}
+                contentContainerStyle={styles.listStyle}
+                showsVerticalScrollIndicator={false}
+                showsHorizontalScrollIndicator={false}></ListView> 
           </View>       
+        );
+    }
+
+    componentDidMount(){
+        this.FetchMovieData();
+    }
+
+    FetchMovieData(){
+        fetch(MOVIEHOT)
+            .then((response)=>response.json())
+            .then((responseData)=>{
+                this.setState({
+                    dataSource:this.state.dataSource.cloneWithRows(responseData.subjects),
+                    isLoad:false,
+                });  
+            }
+        );
+    }
+
+   renderListViewItem(movie){
+        return(
+            <TouchableOpacity
+                style={styles.itemViewStyle} onPress={()=>{this.onMovieClick(movie)}}>
+                <View style={styles.itemViewStyle}>
+                    <Image source={{uri:movie.images.medium}} style={styles.itemIconStyle}></Image>
+                    <Text style={styles.itemTitleStyle}>{movie.title}</Text>  
+                </View>
+            </TouchableOpacity>
+        );
+    }
+
+    onMovieClick(movie){
+        
+    }
+
+    renderLoadingView(){
+        return(
+            <View style={styles.loading}>
+                <Image
+                    source={require('../image/loading.gif')}
+                    style={{height:200,width:200}}></Image>
+            </View>
         );
     }
 
 }
 
+const styles = StyleSheet.create({
+    loading: {
+        flex:1,
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent: 'center',
+        backgroundColor: '#ffffff'
+    },container:{
+        flex:1,
+        backgroundColor: '#ffffff'
+    },listStyle:{
+        flexDirection:'row', 
+        flexWrap:'wrap', 
+    },itemViewStyle:{
+        alignItems:'center',
+        width: width / 3,
+        height:100
+    },
+    itemIconStyle:{
+        width:60,
+        height:60
+    },
+    itemTitleStyle:{
+        marginTop:8
+    }
+});
 
 
 export default MovieHotSearch;
