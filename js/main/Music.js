7ef269fa5185c0706b07343a7955bb62c1aa5559
jack921 <355596943@qq.com>
2017-01-18 01:37:21 +0800

    init

--- js/main/Music.js
+++ js/main/Music.js
@@ -8,20 +8,26 @@ import {
     View,
     Text,
     Image,
-    ListView
+    ListView,
+    TouchableOpacity
 }from 'react-native';
 
+import Loading from './Loading';
 var MUSICTAG=["流行","经典","韩系","欧美"];
 var MUSICURL="https://api.douban.com/v2/music/search?tag=";
 var position=0;
 var data=[];
+var isMore=false;
+
+var Dimensions = require('Dimensions');
+var MyWidth = Dimensions.get('window').width;
 
 class Music extends Component{
 
     constructor(props){
         super(props)
         this.state={
-            isLoad:false,
+            isLoad:true,
             dataSource: new ListView.DataSource({
                 rowHasChanged: (row1, row2) => row1 !== row2
             }), 
@@ -29,7 +35,7 @@ class Music extends Component{
     }
 
     render(){
-       if(!this.state.isLoad){
+       if(this.state.isLoad){
            return this.renderLoadingView();
        } 
        return(
@@ -37,7 +43,8 @@ class Music extends Component{
                 <ListView
                     dataSource={this.state.dataSource}
                     renderRow={this.renderListViewItem.bind(this)}
-                    contentContainerStyle={styles.listStyle}
+                    onEndReached={this.toEnd.bind(this)}
+                    renderFooter={this.renderFooter.bind(this)}
                     showsVerticalScrollIndicator={false}
                     showsHorizontalScrollIndicator={false}></ListView>  
           </View>   
@@ -69,9 +76,45 @@ class Music extends Component{
             <View style={styles.loading}>
                 <Image
                     source={require('../image/loading.gif')}
-                    style={{height:200,width:200}}></Image>
+                    style={{height:200,width:200}}
+                    onEndReached={this.toEnd.bind(this)}
+                    renderFooter={this.renderFooter.bind(this)}></Image>
             </View>
         );
+    }
+
+    renderListViewItem(music){
+        return(
+          <TouchableOpacity
+                activeOpacity={0.8} style={styles.listitem} 
+                onPress={()=>{this.onMovieClick(music)}}>
+           <View style={styles.listitem}>
+                <Image style={styles.itemimage} source={{uri:music.image}}></Image>
+                <View style={styles.itemview}>
+                    <Text style={styles.itemtitle}>{music.title}</Text>
+                    <Text style={styles.itemtext}>{'评分:'+music.rating.average}</Text>
+                    <Text style={styles.itemtext}>{music.author[0].name}</Text>
+                </View>    
+            </View>
+          </TouchableOpacity>
+        );
+    }
+
+    onMovieClick(music){
+
+    }
+
+    renderFooter(movie){
+        return(
+            <Loading style={styles.container}></Loading>
+        )
+    }
+
+    toEnd(movie){
+        if(!this.state.isMore){
+            this.isMore=true;
+            this.FetchMusicData();
+        }
     }
 
 }
@@ -86,6 +129,30 @@ const styles=StyleSheet.create({
         alignItems:'center',
         justifyContent: 'center',
         backgroundColor: '#ffffff'
+    },listitem:{
+        flex:1,
+        flexDirection:'row',
+        backgroundColor:'#ffffff',
+        marginLeft:3,
+        borderBottomWidth:1,
+        borderBottomColor:'gray'
+    },itemimage:{
+        width:110,
+        height:150,
+        marginBottom:8,
+        marginTop:8
+    },itemview:{
+        flex:1,
+        flexDirection:'column',
+        alignItems:'flex-start',
+        marginLeft:10
+    },itemtitle:{
+        fontSize: 22,
+        fontWeight:'bold',
+        textAlign:'left'
+    },itemtext:{
+        fontSize:17,
+        marginTop:5
     }
 });
 
